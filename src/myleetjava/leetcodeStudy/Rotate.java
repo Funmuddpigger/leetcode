af1ceef3cb93b3a8a29e40651784a11d96fd7710
Funmuddpigger <tb963472217@aol.com>
2021-04-09 23:47:02 +0800

    Default Changelist

--- src/myleetjava/leetcodeStudy/Rotate.java
+++ src/myleetjava/leetcodeStudy/Rotate.java
@@ -2,11 +2,13 @@ package myleetjava.leetcodeStudy;
 
 import java.util.Arrays;
 
+//leetcode 189.旋转数组
 public class Rotate {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(rotate2(new int[]{1,2,3,4,5,6,7},3)));
     }
 
+    //mod运算,算出新旧位置,i+k/mod是为防止 > len的情况
     public static int[] rotate(int nums[],int k){
         int len = nums.length;
         int[] res = new int[len];
