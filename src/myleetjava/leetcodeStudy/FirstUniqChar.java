dbf7a14f8597bc552b13459cd58ae10001d03837
Funmuddpigger <tb963472217@aol.com>
2021-04-20 19:45:30 +0800

    Default Changelist

---
+++ src/myleetjava/leetcodeStudy/FirstUniqChar.java
@@ -0,0 +1,25 @@
+package myleetjava.leetcodeStudy;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FirstUniqChar {
+    public static void main(String[] args) {
+        System.out.println(firstUniqChar("dddccdbba"));
+    }
+
+    public static int firstUniqChar(String s) {
+        int len = s.length();
+        Map<Character,Integer> res = new LinkedHashMap<>();
+        for(int i=0;i<len;i++){
+            res.put(s.charAt(i),res.getOrDefault(s.charAt(i),0)+1);
+        }
+        for(int j=0;j<len;j++){
+            //.out.println(res.get(1));
+            if(res.get(s.charAt(j))==1){
+                return j;
+            }
+        }
+        return -1;
+    }
+}
