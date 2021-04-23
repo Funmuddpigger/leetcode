883e1c434365523b528e147abf646a3581bc3048
Funmuddpigger <tb963472217@aol.com>
2021-04-23 21:53:37 +0800

    Default Changelist

---
+++ src/myleetjava/leetcodeStudy/StrStr.java
@@ -0,0 +1,29 @@
+package myleetjava.leetcodeStudy;
+
+public class StrStr {
+    public static void main(String[] args) {
+        System.out.println(strStr("hello", "ll"));
+    }
+
+    public static int strStr(String haystack, String needle) {
+        int haLen = haystack.length(), neLen = needle.length();
+        if (neLen == 0) {
+            return 0;
+        }
+
+        int haLeft = 0, neLeft = 0;
+        while (haLeft < haLen && neLeft < neLen) {
+            if (haystack.charAt(haLeft) == (needle.charAt(neLeft))) {
+                haLeft++;
+                neLeft++;
+            } else {
+                haLeft = haLeft - neLeft + 1;
+                neLeft = 0;
+            }
+            if (neLeft == neLen) {
+                return haLeft - neLeft;
+            }
+        }
+        return -1;
+    }
+}
