0a48d76a84d5356a3b2f142925dca27516617979
v-lijj00 <v-lijj00.gd@chinatelecom.cn>
2022-12-30 18:10:22 +0800

    1. 添加新题目

---
+++ src/myleetjava/leetcodeStudy/LRUCache.java
@@ -0,0 +1,65 @@
+package myleetjava.leetcodeStudy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LRUCache {
+
+    private Map<Integer,Integer> map;
+    private int size;
+    private List<Integer> list;
+
+    public LRUCache(int capacity) {
+        size = capacity;
+        map = new HashMap<>(capacity);
+        list = new ArrayList<>(capacity);
+    }
+
+    public int get(int key) {
+        int i = list.indexOf(key);
+        if(i == -1){
+            return -1;
+        }
+        list.remove(i);
+        list.add(0,key);
+        return map.get(key);
+    }
+
+    public void put(int key, int value) {
+        /**
+         * 超出最小值,去掉,前提:
+         *  1. map不存在该key,否则对应的val更新,不影响原来的size
+         *  2. map.size大于指定的size
+         */
+        if(!map.containsKey(key)&&map.size()>size){
+            //取出最后一个元素key(最久未用的元素)
+            int x = list.get(list.size()-1);
+            //对应的map,list中也移除
+            list.remove(list.size()-1);
+            map.remove(x);
+        }
+        //list已存在key,忽略
+        int i = list.indexOf(key);
+        if(i != -1){
+            list.add(0,key);
+        }else{
+            //已存在,先移除,重新插入到最前面
+            list.remove(i);
+            list.add(0,key);
+        }
+        map.put(key,value);
+    }
+
+    public static void main(String[] args){
+        LRUCache lRUCache = new LRUCache(2);
+
+        lRUCache.put(2, 1); // 缓存是 {1=1}
+        lRUCache.put(1, 1); // 缓存是 {1=1, 2=2}
+        lRUCache.put(2, 3);    // 返回 1
+        lRUCache.put(4, 1); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+        lRUCache.get(1);    // 返回 -1 (未找到)
+        lRUCache.get(2);    // 返回 -1 (未找到)
+    }
+}
---
+++ src/myleetjava/leetcodeStudy/Leetcode43$multiply.java
@@ -0,0 +1,47 @@
+package myleetjava.leetcodeStudy;
+
+public class Leetcode43$multiply {
+
+    public static void main(String[] args) {
+        System.out.println(multiply("123456789","987654321"));
+    }
+
+    public static String multiply(String num1, String num2) {
+        boolean tags = false;
+        char first = num1.charAt(0);
+        char second = num2.charAt(0);
+        if(first == 0 || second == 0){
+            return "0";
+        }else if((first == '-' && second != '-') || (first != '-' && second == '-')){
+            tags = true;
+        }
+
+        if(first == '-'){
+            String[] x = num1.split("-");
+            num1 = x[1];
+        }
+        if(second == '-'){
+            String[] x = num2.split("-");
+            num2 = x[1];
+        }
+
+        String s1 = Integer.toBinaryString(Integer.valueOf(num1));
+        String s2 = Integer.toBinaryString(Integer.valueOf(num2));
+
+        //遍历乘数s2,逢1则被乘数左移--t(遍历次数)-1 位
+        int sum = 0 , t = 0;
+        for(int i = s2.length()-1;i>=0;i--,t++){
+            if(s2.charAt(i)=='1'){
+                int i1 = Integer.parseInt(s1, 2) << t;
+                sum = sum + i1;
+            }
+        }
+        String res;
+        if(tags){
+             res = "-" + sum;
+        }else {
+            res = sum + "";
+        }
+        return res;
+    }
+}
